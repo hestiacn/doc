07962119bc2e8051ef0c2b5487a060a91d4b7df5
meili <12138903+linboxs@user.noreply.gitee.com>
2025-03-04 02:15:27 +0800

    init vitepress project

---
+++ docs/_data/team.js
@@ -0,0 +1,76 @@
+/** @type {import("vitepress").DefaultTheme.TeamMember[]} */
+export const projectManagers = [
+	{
+		avatar: '/images/avatar/ScIT-Raphael.png',
+		name: 'Raphael Schneeberger 🇨🇭',
+		title: 'Project Founder',
+		links: [{ icon: 'github', link: 'https://github.com/ScIT-Raphael' }],
+	},
+	{
+		avatar: '/images/avatar/jaapmarcus.png',
+		name: 'Jaap Marcus 🇳🇱',
+		links: [
+			{ icon: 'github', link: 'https://github.com/jaapmarcus' },
+			{ icon: 'twitter', link: 'https://twitter.com/jaapmarcus' },
+		],
+	},
+	{
+		avatar: '/images/avatar/Lupul.png',
+		name: 'Robert Zollner 🇷🇴',
+		links: [{ icon: 'github', link: 'https://github.com/Lupul' }],
+	},
+];
+
+const globeIcon = {
+	svg: '🌐',
+};
+
+/** @type {import("vitepress").DefaultTheme.TeamMember[]} */
+export const teamMembers = [
+	{
+		avatar: '/images/avatar/jakobbouchard.jpg',
+		name: 'Jakob Bouchard 🇨🇦',
+		title: 'Developer',
+		org: 'Prosomo',
+		orgLink: 'https://prosomo.com',
+		links: [
+			{ icon: 'github', link: 'https://github.com/jakobbouchard' },
+			{ icon: 'linkedin', link: 'https://www.linkedin.com/in/jakobbouchard' },
+			{ icon: globeIcon, link: 'https://jakobbouchard.dev' },
+		],
+	},
+	{
+		avatar:'/images/avatar/32452f6.png',
+		name: 'Falzo 🇩🇪',
+		links: [{ icon: 'github', link: 'https://github.com/falzoMAD' }],
+	},
+	{
+		avatar: '/images/avatar/divinity76.png',
+		name: 'divinity76 🇳🇴',
+		links: [{ icon: 'github', link: 'https://github.com/divinity76' }],
+	},
+	{
+		avatar: '/images/avatar/Pleskan.png',
+		name: 'Anton Pleskanovskyy 🇺🇦',
+		links: [{ icon: 'github', link: 'https://github.com/Pleskan' }],
+	},
+	{
+		avatar: '/images/avatar/AlecRust.png',
+		name: 'Alec Rust 🇬🇧',
+		links: [
+			{ icon: 'github', link: 'https://github.com/AlecRust' },
+			{ icon: globeIcon, link: 'https://www.alecrust.com/' },
+		],
+	},
+	{
+		avatar: '/images/avatar/ioannidesalex.png',
+		name: 'Alexandros Ioannides 🇨🇾',
+		title: 'CTO',
+		org: 'FocusNet',
+		orgLink: 'https://focus-net.net',
+		links: [
+			{ icon: 'github', link: 'https://github.com/ioannidesalex' },
+			{ icon: 'linkedin', link: 'https://www.linkedin.com/in/ioannidesa' },
+		],
+	},
+];
